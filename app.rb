5c7ea204e9799bdec7742f5304f1cc619bff857c
LeonardoLG <leonardoleongutierrez27@gmail.com>
2020-11-02 09:50:03 -0500

    Sprint 2

--- app.rb
+++ app.rb
@@ -3,13 +3,18 @@ require './config'
 require './lib/Ahorcado'
 
 ahorcado= nil
+get '/secreto' do
+    ahorcado=Ahorcado.new(params[:p])
+    @palabra= ahorcado.mostrarGuiones()
+    erb :index
+end
 get '/' do
-    ahorcado=Ahorcado.new
-    @palabra= ahorcado.palabra
+    ahorcado=Ahorcado.new("A")
+    @palabra= ahorcado.mostrarGuiones()
     erb :index
 end
 
 post '/' do
-    @mensaje=ahorcado.arriesgar('A')
+    @mensaje=ahorcado.arriesgar(params[:letra])
     erb :index
 end
--- lib/Ahorcado.rb
+++ lib/Ahorcado.rb
@@ -1,10 +1,26 @@
 class Ahorcado
     attr_reader :palabra
-    def initialize
+    def initialize(palabraSecreta)
         @palabra="_"
+        @palabraSecreta=palabraSecreta
     end
     def arriesgar(letra)
-        return "Ganaste"
+        if @palabraSecreta.eql? letra
+            return "Ganaste"            
+        else
+            return "Perdiste"
+        end
+    end
+
+    def mostrarGuiones()
+        cantidadLetras = @palabraSecreta.length
+        guiones = "_"
+        for i in (1..cantidadLetras-1)  do
+            
+            guiones = guiones.concat(" _");
+
+        end
+        return guiones       
     end
 
 end
